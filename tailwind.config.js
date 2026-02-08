97f20a87d3c264d7691a529884af2834cd5d6b33
666 <lll2120651866@163.com>
2026-02-08 22:34:16 +0800

    Fix Vercel deployment: add Hugo version and Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./content/**/*.{html,js,md}",
+    "./layouts/**/*.{html,js}",
+    "./themes/digio-theme/layouts/**/*.{html,js}",
+    "./themes/digio-theme/content/**/*.{html,js,md}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
